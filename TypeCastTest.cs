708d5f78513ae07c1380a7a0a34abb9cb5b68582
prozoroff <alexprozoroff@gmail.com>
2014-05-22 11:41:04 +0400

    Update TypeCastTest.cs

--- TypeCastTest.cs
+++ TypeCastTest.cs
@@ -6,83 +6,91 @@ using System.Text;
 
 namespace TypeCastTest
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            foreach(var a in new Action[]{VirtualCallTest, AsCastTest, ExplicitCastTest})
-            {
-                var stopwatch = new Stopwatch();
-                stopwatch.Start();
-                a.Invoke();
-                stopwatch.Stop();
-                Console.WriteLine(stopwatch.ElapsedTicks.ToString());
-            }
+	class Program
+	{
+		static long[] result = new long[]{0,0,0};
+		static Action[] methods = new Action[]{ VirtualCallTest, AsCastTest, ExplicitCastTest };
 
-            Console.ReadKey();
+		static int iterCount = 100000;
+		static int measureCount = 1000;
 
-        }
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < measureCount; i++) 
+			{
+				for (int m = 0; m < 3; m++) 
+				{
+					result [m] += measureTicks (methods [m]);
+				}
+			}
 
-        static int IterCount = 10000;
-        static A a = new B();
-        static A b = new B();
+			Console.WriteLine ("Virtual Call: " + ((long)(result [0] / measureCount)).ToString ());
+			Console.WriteLine ("'As' method: " + ((long)(result [1] / measureCount)).ToString ());
+			Console.WriteLine ("'()' method: " + ((long)(result [2] / measureCount)).ToString ());
+			Console.ReadKey ();
 
-        static void VirtualCallTest()
-        {
-            for (int i = 0; i < IterCount; i++)
-                b = a.GetB();
-        }
+		}
 
-        static void AsCastTest()
-        {
-            for (int i = 0; i < IterCount; i++)
-                b = a as B;
-        }
+		static long measureTicks(Action method)
+		{
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+			method.Invoke();
+			stopwatch.Stop();
+			return stopwatch.ElapsedTicks;
+		}
+			
+		static A a = new B();
+		static A b = new B();
 
-        static void ExplicitCastTest()
-        {
-            for (int i = 0; i < IterCount; i++)
-                b = (B)a;
-        }
+		static void VirtualCallTest()
+		{
+			for (int i = 0; i < iterCount; i++)
+				b = a.GetB();
+		}
 
-        class A
-        {
-            protected int _i;
+		static void AsCastTest()
+		{
+			for (int i = 0; i < iterCount; i++)
+				b = a as B;
+		}
 
-            public virtual B GetB() { return null; }
-        }
+		static void ExplicitCastTest()
+		{
+			for (int i = 0; i < iterCount; i++)
+				b = (B)a;
+		}
 
-        class B : A, ICloneable
-        {
-            public B()
-            {
-                _a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-                _s = "some test string";
-                _t = DateTime.Now;
-            }
+		class A
+		{
+			protected int _i;
 
-            public B(B b)
-            {
-                _a = b._a;
-                _s = b._s;
-                _i = b._i;
-                _t = b._t;
-            }
+			public virtual B GetB() { return null; }
+		}
 
-            public int[] _a;
-            string _s;
-            DateTime _t;
+		class B : A
+		{
+			public B()
+			{
+				_a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+				_s = "some test string";
+				_t = DateTime.Now;
+				_i = 0;
+			}
 
-            public override B GetB() { return this; }
+			public B(B b)
+			{
+				_a = b._a;
+				_s = b._s;
+				_i = b._i;
+				_t = b._t;
+			}
 
-            #region ICloneable Members
-            public object Clone()
-            {
-                return MemberwiseClone();
-            }
-            #endregion
-        }
+			public int[] _a;
+			string _s;
+			DateTime _t;
 
-
-    }
+			public override B GetB() { return this; }
+		}
+	}
 }
